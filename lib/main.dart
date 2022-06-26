e21d80aeb9be10f5904d93bf9995c4b48579b178
SunkenInTime <shawnadedeji@gmail.com>
2022-06-26 02:28:41 -0400

    Finished register screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,140 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(
-      primarySwatch: Colors.blue,
+      primaryColor: const Color.fromARGB(255, 31, 31, 31),
     ),
     home: const HomePage(),
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    const double sizedBoxWidth = 250;
     return Scaffold(
+        backgroundColor: const Color.fromARGB(255, 31, 31, 31),
         appBar: AppBar(
           title: const Text("Register"),
+          backgroundColor: const Color.fromARGB(255, 107, 65, 114),
         ),
-        body: Column(
-          children: [
-            TextField(),
-            TextField(),
-            TextButton(
-                onPressed: () async {
-                  print("John");
-                },
-                child: const Text('Register')),
-          ],
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //Email
+                      SizedBox(
+                        width: sizedBoxWidth,
+                        child: TextField(
+                          controller: _email,
+                          enableSuggestions: true,
+                          autocorrect: false,
+                          keyboardType: TextInputType.emailAddress,
+                          style: const TextStyle(color: Colors.white),
+                          textAlign: TextAlign.center,
+                          decoration: const InputDecoration(
+                            hintText: "Email",
+                            hintStyle: TextStyle(color: Colors.white),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.blue, width: 2),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 167, 167, 167),
+                                  width: 2),
+                            ),
+                            border: OutlineInputBorder(),
+                            //focusedBorder:OutlineInputBorder(borderSide: BorderSide(width: 1))
+                          ),
+                        ),
+                      ),
+
+                      //Spacing
+                      const SizedBox(height: 10),
+
+                      //Password
+                      SizedBox(
+                        width: sizedBoxWidth,
+                        child: TextField(
+                          obscureText: true,
+                          enableSuggestions: false,
+                          autocorrect: false,
+                          controller: _password,
+                          style: const TextStyle(color: Colors.white),
+                          textAlign: TextAlign.center,
+                          decoration: const InputDecoration(
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(),
+                            hintText: "Password",
+                            focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.blue, width: 2),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color.fromARGB(255, 167, 167, 167),
+                                  width: 2),
+                            ),
+                            hintStyle: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+
+                      TextButton(
+                          onPressed: () async {
+                            final email = _email.text;
+                            final password = _password.text;
+                            final userCredential = await FirebaseAuth.instance
+                                .createUserWithEmailAndPassword(
+                                    email: email, password: password);
+                            // ignore: avoid_print
+                            print(userCredential);
+                          },
+                          child: const Text('Register')),
+                    ],
+                  ),
+                );
+              default:
+                return const Text("Loading...");
+            }
+          },
         ));
   }
 }
