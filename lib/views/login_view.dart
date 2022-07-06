8670344533ef01ce371698b4732443abb77d4a22
SunkenInTime <shawnadedeji@gmail.com>
2022-07-06 14:34:44 -0400

    Revamp

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import "dart:developer" as devtools show log;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -121,11 +123,18 @@ class _LoginViewState extends State<LoginView> {
                     await FirebaseAuth.instance.signInWithEmailAndPassword(
                         email: email, password: password);
 
-                    if (!man) {} //used this to escape an error i have no idea how to fix :skull:
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      notesRoute,
-                      (route) => false,
-                    );
+                    final user = FirebaseAuth.instance.currentUser;
+                    if (user?.emailVerified ?? false) {
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        notesRoute,
+                        (route) => false,
+                      );
+                    } else {
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        verifyRoute,
+                        (route) => false,
+                      );
+                    }
                   } on FirebaseAuthException catch (e) {
                     if (e.code == "user-not-found") {
                       await showErrorDialog(
