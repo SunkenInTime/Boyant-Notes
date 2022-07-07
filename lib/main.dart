6ed7649c89f63ea238b97b6b805efda88fd42541
SunkenInTime <shawnadedeji@gmail.com>
2022-07-07 13:40:13 -0400

    Changed loading screen for ios and android

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/firebase_options.dart';
@@ -7,6 +10,9 @@ import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
 import 'package:mynotes/views/verify_email_view.dart';
 import "dart:developer" as devtools show log;
+
+const double sizedBoxWidth = 300;
+const double sizedBoxHeight = 300;
 
 //Creates an empty sized box for sapce
 SizedBox createSpace(double height) {
@@ -54,7 +60,23 @@ class HomePage extends StatelessWidget {
             }
 
           default:
-            return const CircularProgressIndicator();
+            dynamic loadingCircle;
+            if (Platform.isIOS) {
+              devtools.log("We are on IOS");
+              loadingCircle = const CupertinoActivityIndicator();
+            } else {
+              loadingCircle = const CircularProgressIndicator();
+            }
+            return Scaffold(
+              backgroundColor: const Color.fromARGB(255, 180, 180, 180),
+              body: Center(
+                child: SizedBox(
+                  width: sizedBoxWidth,
+                  height: sizedBoxHeight,
+                  child: Center(child: loadingCircle),
+                ),
+              ),
+            );
         }
       },
     );
