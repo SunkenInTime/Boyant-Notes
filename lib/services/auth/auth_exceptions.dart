f0792744eacf3dfcd2556790dade7e0fcda3dd96
SunkenInTime <shawnadedeji@gmail.com>
2022-07-07 14:41:45 -0400

    Auth Class

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,18 @@
+//Login Exceptions
+class UserNotFoundAuthException implements Exception {}
+
+class WrongPasswordAuthException implements Exception {}
+
+//Register Exceptions
+class WeakPasswordAuthException implements Exception {}
+
+class EmailAlreadyInUseAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
+
+//Generic Exceptions
+class ConnectionFailedAuthException implements Exception {}
+
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
