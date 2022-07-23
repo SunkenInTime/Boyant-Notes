0274ab412333e77ba6f2d3c3ccdb84b1678f5f5c
SunkenInTime <shawnadedeji@gmail.com>
2022-07-22 23:48:14 -0400

    Notes Service

---
+++ lib/services/crud/notes_service.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart'
+    show getApplicationDocumentsDirectory;
+import 'package:path/path.dart' show join;
+
+@immutable
+class DatabaseUser {
+  final int id;
+  final String email;
+
+  const DatabaseUser({required this.id, required this.email});
+
+  DatabaseUser.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        email = map[emailColumn] as String;
+
+  @override
+  String toString() => "Person, ID = $id, email = $email";
+
+  @override
+  bool operator ==(covariant DatabaseUser other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+}
+
+class DatabaseNote {
+  final int id;
+  final int userId;
+  final String text;
+  final bool isSyncedWithCloud;
+
+  DatabaseNote(
+      {required this.id,
+      required this.userId,
+      required this.text,
+      required this.isSyncedWithCloud});
+  DatabaseNote.fromRow(Map<String, Object?> map)
+      : id = map[idColumn] as int,
+        userId = map[userIdColumn] as int,
+        text = map[textColumn] as String,
+        isSyncedWithCloud = (map[isSynced] as int) == 1 ? true : false;
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return super.toString();
+  }
+}
+
+const idColumn = "id";
+const emailColumn = "email";
+const userIdColumn = "user_id";
+const textColumn = "text";
+const isSynced = "is_synced";
