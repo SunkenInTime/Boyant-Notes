e56739eab3ec153bd750f243acfd3b6a91332423
SunkenInTime <shawnadedeji@gmail.com>
2022-08-03 23:51:40 -0400

    Hot fix

--- lib/views/todo/todo_view.dart
+++ lib/views/todo/todo_view.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../../constants/routes.dart';
+import '../../enums/menu_action.dart';
+import '../../main.dart';
+import '../../services/auth/auth_service.dart';
+import '../main_ui.dart';
+
 class TodoView extends StatefulWidget {
   const TodoView({Key? key}) : super(key: key);
 
@@ -11,7 +17,45 @@ class _TodoViewState extends State<TodoView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Text("Hello"),
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        title: const Text("Todo"),
+        backgroundColor: themeColor,
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+
+                  if (shouldLogout) {
+                    await AuthService.firebase().logOut();
+                    if (!man) {} //used this to escape an error i have no idea how to fix :skull:
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      loginRoute,
+                      (_) => false,
+                    );
+                  }
+                  break;
+              }
+            },
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Text("Log out"),
+                )
+              ];
+            },
+          )
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          "Todo list here",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
     );
   }
 }
